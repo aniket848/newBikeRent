fa69cf860b7da6a5551a57dbb79677f9070fce6c
aniket848 <62254194+aniket848@users.noreply.github.com>
2025-01-14 10:14:16 +0530

    Update helper.js

--- client/src/components/helper.js
+++ client/src/components/helper.js
@@ -1,3 +1,3 @@
-const BASE_URL = "http://localhost:4000";
-// const BASE_URL = "https://bikeserver.onrender.com";
+//const BASE_URL = "http://localhost:4000";
+const BASE_URL = "https://bikerentbackend.onrender.com";
 export default BASE_URL;
